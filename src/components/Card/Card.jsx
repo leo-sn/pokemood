8b8acb6a269e348dd609278a1b52ec21b8b5f4e9
unknown <umairshabbir178@gmail.com>
2023-01-07 10:45:11 -0800

    umair commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,12 @@
+import img1 from '../../images/pokemon card.png'
+import './Card.css'
 const Card = () => {
 
     return (
         <div>
-            HERE IS THE CARD
+            <p>HERE IS THE CARD</p>
+            <img src={img1}></img>
+            <img className='card__pokemon' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/100.gif'></img>
         </div>
     )
 }
