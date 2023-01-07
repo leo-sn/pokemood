78bd8c0f8133ccba0f96cf752b4ae0437f753db0
unknown <umairshabbir178@gmail.com>
2023-01-07 11:41:27 -0800

    umair second commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,6 @@ const Card = () => {
 
     return (
         <div>
-            <p>HERE IS THE CARD</p>
             <img src={img1}></img>
             <img className='card__pokemon' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/100.gif'></img>
         </div>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,29 +1,33 @@
+import './Form.css'
+import Card from '../Card/Card'
 const Form = () => {
 
     const feelings = ['Happy','Sad','Excited','Angry','Glommy']
 
     return (
-        <>
-        <form>
-            <label className="question">What is your name?</label>
-            <input type="text" name='name'  required></input>
+        <div className='form__container'>
+            <form>
+                <label className="question">What is your name?</label>
+                <input type="text" name='name'  required></input>
 
-            <label className="question">How are you feeling?</label>
-                <div className="feelings-container">
-                    {
-                        feelings.map((f) => {
-                            return (
-                                <>
-                                    <label>{f}</label>
-                                    <input type='radio'></input>
-                                </>
-                            )
-                        })
-                    }
-                </div>
-            <button type="submit">Submit</button>
-        </form>
-        </>
+                <label className="question">How are you feeling?</label>
+                    <div className="feelings-container">
+                        {
+                            feelings.map((f) => {
+                                return (
+                                    <>
+                                        
+                                        <input type='radio' name='mo'></input>
+                                        <label>{f}</label>
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
+                <button type="submit">Submit</button>
+            </form>
+            <Card></Card>
+        </div>
     )
 }
 
